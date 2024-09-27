6012371425e4c79e38a888920c603872b842ed0f
Zoe Faltibà <zoefaltiba@gmail.com>
2024-09-27 10:53:36 +0200

    fix back_and_forth in witness mode

--- tests/utils/mod.rs
+++ tests/utils/mod.rs
@@ -11,6 +11,7 @@ pub const ESPLORA_REGTEST_URL: &str = "http://127.0.0.1:8094/regtest/api";
 pub const ESPLORA_MAINNET_URL: &str = "https://blockstream.info/api";
 pub const FAKE_TXID: &str = "e5a3e577309df31bd606f48049049d2e1e02b048206ba232944fcc053a176ccb:0";
 pub const UDA_FIXED_INDEX: u32 = 0;
+pub const DEFAULT_FEE_ABS: u64 = 400;
 
 pub use std::{
     cell::OnceCell,
