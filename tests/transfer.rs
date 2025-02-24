a8b34b4a41331c86d691f8188105185c4618d8d9
will-bitlightlabs <jayabb@bitlightlabs.com>
2025-02-24 15:13:44 +0800

    feat(tests): Add test cases accept_0conf and same_transfer_twice_no_update_witnesses
    Signed-off-by: will-bitlightlabs <jayabb@bitlightlabs.com>

--- tests/transfer.rs
+++ tests/transfer.rs
@@ -462,3 +462,142 @@ fn transfer_loop(
         vec![amount_3 - amount_5, amount_7],
     );
 }
+
+// TODO: The `update_witnesses` method has been removed in RGB V0.12,
+// So this test case may need to be removed or redesigned
+// Need to sync with Dr. Maxim
+#[rstest]
+#[ignore = "fix needed"]
+#[case(TransferType::Blinded)]
+#[case(TransferType::Witness)]
+fn same_transfer_twice_update_witnesses(#[case] transfer_type: TransferType) {}
+
+#[rstest]
+#[case(TransferType::Blinded)]
+#[case(TransferType::Witness)]
+fn same_transfer_twice_no_update_witnesses(#[case] transfer_type: TransferType) {
+    println!("transfer_type {transfer_type:?}");
+
+    // TODO: This test case aims to verify if asset transfers are handled correctly when using RBF (Replace-By-Fee) with the same invoice
+    // In RGB V0.11, there was an inflation attack vulnerability where using RBF with the same invoice would cause the receiver to record two receive states,
+    // while the sender only paid the assets once. This resulted in the total circulating assets exceeding the issued amount.
+    //
+    // In RGB V0.12, since it's not possible to use RBF with the same invoice, we cannot test for this inflation attack.
+    // NOTE: When paying the same invoice for the second time, the error `called `Result::unwrap()` on an `Err` value: Fulfill(StateInsufficient)` occurs
+    // Need to consult with Dr. Maxim on how to construct RBF asset transfer examples using the current API.
+
+    initialize();
+
+    let mut wlt_1 = get_wallet(&DescriptorType::Wpkh);
+    let mut wlt_2 = get_wallet(&DescriptorType::Wpkh);
+
+    let issue_supply = 2000;
+    // Create and issue NIA asset
+    let mut params = NIAIssueParams::new("TestAsset", "TEST", "centiMilli", issue_supply);
+    let outpoint = wlt_1.get_utxo(None);
+    params.add_allocation(outpoint, issue_supply);
+    let contract_id = wlt_1.issue_nia_with_params(params);
+    wlt_1.send_contract("TestAsset", &mut wlt_2);
+    wlt_2.reload_runtime();
+
+    let amount = 100;
+    let wout = match transfer_type {
+        TransferType::Blinded => false,
+        TransferType::Witness => true,
+    };
+    let invoice = wlt_2.invoice(contract_id, amount, wout, Some(0));
+    let _ = wlt_1.transfer(invoice.clone(), None, Some(500), false, None);
+
+    dbg!(wlt_1
+        .runtime()
+        .state_all(None)
+        .map(|(c, s)| { s.owned })
+        .collect::<Vec<_>>());
+    dbg!(wlt_1
+        .runtime()
+        .state_own(None)
+        .map(|(c, s)| { s.owned })
+        .collect::<Vec<_>>());
+
+    // dbg!(wlt_2
+    //     .runtime()
+    //     .state_all(None)
+    //     .map(|(c, s)| { s.owned })
+    //     .collect::<Vec<_>>());
+    // dbg!(wlt_2
+    //     .runtime()
+    //     .state_own(None)
+    //     .map(|(c, s)| { s.owned })
+    //     .collect::<Vec<_>>());
+
+    // TODO: called `Result::unwrap()` on an `Err` value: Fulfill(StateInsufficient)
+    let (consignment, _) = wlt_1.transfer(invoice, None, Some(1000), true, None);
+
+    wlt_2.accept_transfer(&consignment, None);
+
+    let wlt_2_contract_state = wlt_2.runtime().state_own(None).map(|s| s.1.owned);
+    dbg!(wlt_2_contract_state.collect::<Vec<_>>());
+
+    wlt_2.check_allocations(contract_id, AssetSchema::Nia, vec![amount]);
+
+    wlt_2.send(&mut wlt_1, wout, contract_id, amount, 1000, None, None);
+
+    let wlt_1_contract_state = wlt_1.runtime().state_own(None).map(|s| s.1.owned);
+    dbg!(wlt_1_contract_state.collect::<Vec<_>>());
+
+    let mut wlt_3 = get_wallet(&DescriptorType::Wpkh);
+    wlt_1.send_contract("TestAsset", &mut wlt_3);
+    wlt_3.reload_runtime();
+
+    wlt_1.send(
+        &mut wlt_3,
+        wout,
+        contract_id,
+        issue_supply,
+        1000,
+        None,
+        None,
+    );
+    let wlt_3_contract_state = wlt_3.runtime().state_own(None).map(|s| s.1.owned);
+    dbg!(wlt_3_contract_state.collect::<Vec<_>>());
+}
+
+#[test]
+fn accept_0conf() {
+    initialize();
+
+    let mut wlt_1 = get_wallet(&DescriptorType::Wpkh);
+    let mut wlt_2 = get_wallet(&DescriptorType::Wpkh);
+
+    let issue_supply = 600;
+    // Create and issue NIA asset
+    let mut params = NIAIssueParams::new("TestAsset", "TEST", "centiMilli", issue_supply);
+    let outpoint = wlt_1.get_utxo(None);
+    params.add_allocation(outpoint, issue_supply);
+    let contract_id = wlt_1.issue_nia_with_params(params);
+    wlt_1.send_contract("TestAsset", &mut wlt_2);
+    wlt_2.reload_runtime();
+
+    let amt = 200;
+    let invoice = wlt_2.invoice(contract_id, amt, true, Some(0));
+    let (consignment, tx) = wlt_1.transfer(invoice.clone(), None, None, true, None);
+    let txid = tx.txid();
+
+    wlt_2.accept_transfer(&consignment, None);
+
+    // wlt_2 sees the allocation even if TX has not been mined
+    wlt_2.check_allocations(contract_id, AssetSchema::Nia, vec![amt]);
+
+    wlt_1.sync();
+
+    let wlt_1_change_amt = issue_supply - amt;
+
+    // wlt_1 needs to get tentative allocations to see its change from the unmined TX
+    let wlt_1_contract_state = wlt_1.runtime().state_own(None).map(|s| s.1.owned);
+    dbg!(wlt_1_contract_state.collect::<Vec<_>>());
+
+    // after mining, wlt_1 doesn't need to get tentative allocations to see the change
+    wlt_1.mine_tx(&txid, false);
+    wlt_1.sync();
+    wlt_1.check_allocations(contract_id, AssetSchema::Nia, vec![wlt_1_change_amt]);
+}
